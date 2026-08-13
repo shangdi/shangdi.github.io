680b7da529ccee71201cfbe781170e1584916145
Wildan <mymultipurposecontact@gmail.com>
2026-08-13 11:49:14 +0700

    add + modify background wall & add gallery pics

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -40,6 +40,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
 			"assets/images/DesktopWallpaper/front.avif",
+			"assets/images/DesktopWallpaper/invokedchaos.avif",
 			"assets/images/DesktopWallpaper/leaves.avif",
 			"assets/images/DesktopWallpaper/swimmingcat.avif",
 			"assets/images/DesktopWallpaper/greeny.avif",
@@ -47,10 +48,11 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/hasil1.avif",
-			"assets/images/MobileWallpaper/hasil2.avif",
-			"assets/images/MobileWallpaper/hasil3.avif",
-			"assets/images/MobileWallpaper/hasil4.avif",
+			"assets/images/MobileWallpaper/front.avif",
+			"assets/images/MobileWallpaper/invokedchaos.avif",
+			"assets/images/MobileWallpaper/leaves.avif",
+			"assets/images/MobileWallpaper/swimmingcat.avif",
+			"assets/images/MobileWallpaper/greeny.avif",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
